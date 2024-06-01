2dcbdd9e4e91191f204a336c02b9f77398ab4f8d
Luka Ivanović <luka@ljudi.org>
2024-06-01 12:12:33 +0200

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,9 +19,35 @@ func main() {
 		if len(args) == 0 {
 			continue
 		}
-		switch args[0] {
+		command, args, _ := next(args)
+		switch command {
+		case "exit":
+			exit_code_str, _, err := next(args)
+			if err != nil {
+				os.Exit(0)
+			}
+			exit_code, err := strconv.Atoi(exit_code_str)
+			if err != nil {
+				panic(err)
+			}
+			os.Exit(exit_code)
 		default:
-			fmt.Fprintf(os.Stdout, "%s: command not found\n", args[0])
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 		}
 	}
+}
+
+func next(ss []string) (string, []string, error) {
+	if len(ss) == 0 {
+		var z string
+		return z, ss, errors.New("slice is empty")
+	}
+	return ss[0], ss[1:], nil
+}
+
+func peek(ss []string) (string, error) {
+	if len(ss) == 0 {
+		return "", errors.New("lice is empty")
+	}
+	return ss[0], nil
 }
